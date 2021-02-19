79637a4d0a684421f034668f5814e4194e26e494
Alfian Busyro <arufian703@gmail.com>
2021-02-19 23:20:48 +0900

    ok

--- routes/root.js
+++ routes/root.js
@@ -4,8 +4,8 @@ module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { hello: 'world' }
   })
-  fastify.listen(process.env.PORT, '0.0.0.0', (err, address) => {
-    if (err) throw err
-    fastify.log.info(`server listening on ${address}`)
-  })
+  // fastify.listen(process.env.PORT, '0.0.0.0', (err, address) => {
+  //   if (err) throw err
+  //   fastify.log.info(`server listening on ${address}`)
+  // })
 }
